b1156523dd32984ab82731fe2971b12e8bd92104
Toufa7 <otoufah@student.1337.ma>
2023-10-11 11:58:55 +0100

    A

--- requirements/wordpress/tools/wordpress_requirements.sh
+++ requirements/wordpress/tools/wordpress_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sleep 10
 # Dependencies
 apt install -y wget net-tools vim mariadb-client iputils-ping sudo php7.4-fpm php-mysql
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
@@ -11,10 +11,6 @@ sleep 5
 cd /var/www/html/wordpress
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
-DIRI="Build"
-if [ ! -d "$DIRI" ]; then
-    sudo mkdir $DIRI
-fi
 sudo wp core download --allow-root
 echo "[SUCCESS: Wordpress-CLI]"
 sleep 5
@@ -26,21 +22,17 @@ if [ ! -d "$DIR" ]; then
 fi
 
 
-service php7.4-fpm start
-
-# # Wordpress Configuration Settings
+# Wordpress Configuration Settings
 wp core config --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$WP_DB_HOST --path=/var/www/html/wordpress --allow-root
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 echo -e "[SUCCESS: WORDPRESS (wp-config.php, admin, user)]"
 sleep 5
 
-service php7.4-fpm stop
-
 # Verifying user and admin has been successfully created
 wp core is-installed --allow-root
 wp user list --allow-root
-# sleep 5
+sleep 10
 
 # mysql -h mariadb -u user1 -pus@r123
 
